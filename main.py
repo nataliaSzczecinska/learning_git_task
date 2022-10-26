c2f0b0f1ce8c13fd0a2369a21e7e660a8e84fb7b
nataliaSzczecinska <nataliaszczecinska@o2.pl>
2022-10-26 17:47:34 +0200

    Finish task

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ shopping_list = {
 
 number_of_products = 0
 print("Shopping list")
+for key, value in shopping_list.items():
+    values = ', '.join((str(element.title()) for element in value))
+    print(f"I am going to {key.title()} and buy there [{values}]")
+    number_of_products = number_of_products + len(value)
+print(f"I will buy {number_of_products} products")
